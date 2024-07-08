31145786fe72e37138d4b51d4938f6a6f166a337
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2024-07-08 19:12:33 +0300

    create bot

---
+++ bot/main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from credentials import *
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    pass
+  
+
+bot.run(BOT_TOKEN)
