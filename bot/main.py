4c952dc6a57bd59a757c2af7fdf9f4841788dd15
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2024-07-17 19:59:52 +0300

    refactor

--- bot/main.py
+++ bot/main.py
@@ -34,15 +34,17 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     channel_id = message.channel.id
-    if message.mentions and channel_id != PING_CHAT_ID and channel_id not in EXCLUDE_CHAT_IDS:
-        action = PING_MESSAGE_WITH_TEXT_ACTION if remove_mentions(message.content) else PING_MESSAGE_ACTION
-        new_message = generate_message(NEW_MESSAGE_PATTERN, message)
-        match action:
-            case ACTION.MOVE:
-                await message.delete()
-                await ping_channel.send(new_message)
-            case ACTION.COPY:
-                await ping_channel.send(new_message)
+    if not message.mentions or channel_id == PING_CHAT_ID or channel_id in EXCLUDE_CHAT_IDS:
+        return
+    
+    action = PING_MESSAGE_WITH_TEXT_ACTION if remove_mentions(message.content) else PING_MESSAGE_ACTION
+    new_message = generate_message(NEW_MESSAGE_PATTERN, message)
+    match action:
+        case ACTION.MOVE:
+            await message.delete()
+            await ping_channel.send(new_message)
+        case ACTION.COPY:
+            await ping_channel.send(new_message)
 
 
 bot.run(BOT_TOKEN)
