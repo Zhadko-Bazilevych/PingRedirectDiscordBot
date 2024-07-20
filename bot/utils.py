01d9e823db4c301a5a9b41166f4fb1dcbfb0378b
Zhadko-Bazilevych <serhii.zhadkobazilevych@gmail.com>
2024-07-20 13:45:06 +0300

    mentions detection update

--- bot/utils.py
+++ bot/utils.py
@@ -1,7 +1,7 @@
 import re
 
 def remove_mentions(text):
-    return re.sub(r'<[^>]*>', '', text)
+    return re.sub(r'<[^>]*>', '', text).strip()
 
 def generate_message(pattern, message):
     return pattern.replace('{message}', message.content).replace('{author}', message.author.display_name)
